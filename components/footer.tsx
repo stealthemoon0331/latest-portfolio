0235a590a92fb3fcf8b45cf084e6bb46132e548d
stealthemoon0331 <tatsuyakuroda60@gmail.com>
2025-11-01 02:25:59 +0900

    Comment out unused imports in footer.tsx to clean up the codebase.

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FaLocationArrow } from "react-icons/fa6";
+// import { FaLocationArrow } from "react-icons/fa6";
 
-import { MagicButton } from "@/components/ui/magic-button";
-import { links } from "@/config";
+// import { MagicButton } from "@/components/ui/magic-button";
+// import { links } from "@/config";
 import { socialMedia } from "@/data";
 
 export const Footer = () => {
